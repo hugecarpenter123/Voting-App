85c62f5aba287cd70962439a2c2381e538461f3c
Arkadiusz Kupiec <arkkupi85@gmail.com>
2022-11-02 11:55:05 +0100

    Create main & detail pages along with CSS

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
+
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'polls/index.html')
+    question_objects = Question.objects.all()
+    context = {
+        'question_objects': question_objects,
+    }
+    return render(request, 'polls/index.html', context)
+
+def polls(request, pk):
+    question_objects = Question.objects.all()
+
+    context = {
+        'question_objects': question_objects,
+        'question_object': question_objects.get(pk=pk),
+        'pk': pk,
+    }
+    return render(request, 'polls/list.html', context)
